0d00961ecfaf1e48c5661d5b47d994d24ef401ad
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2023-05-22 17:07:25 +0200

    Añadido esqueleto Spring Boot

---
+++ src/test/java/com/ciber12/calculator12/Calculator12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber12.calculator12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
