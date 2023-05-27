a18e805b530f0361389eb6801325ffe4f3bd75ac
qyo21647 <101132707+qyo21647@users.noreply.github.com>
2023-05-27 14:41:36 +0200

    Delete calculator12 directory

--- calculator12/src/main/java/com/ciber12/calculator12/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.ciber12.calculator12;
-import org.springframework.stereotype.Service;
-
-@Service
-public class Calculator {
-    int sum(int a, int b) {
-        return a + b;
-        }
-}
